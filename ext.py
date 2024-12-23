e9e6f13d1dfda42360d579c6daf1e23ab36e9349
zana-nouri <zananouri2@gmail.com>
2024-12-24 01:23:04 +0330

    sgfdsgsfdh

--- ext.py
+++ ext.py
@@ -5,3 +5,4 @@ from flask_login import LoginManager
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+safsaf
--- forms.py
+++ forms.py
@@ -3,15 +3,14 @@
 from __future__ import unicode_literals
 from flask_wtf import FlaskForm
 from wtforms import RadioField, SubmitField, StringField, PasswordField
-from wtforms.validators import DataRequired, Length
-
+from wtforms.validators import DataRfequired, Length
+safas
 class TodoListForm(FlaskForm):
-    title = StringField('标题', validators=[DataRequired(), Length(1, 64)])
+    title = StringField('标题', validatorsasfsafDataRequired(), Length(1, 64)])
     status = RadioField('是否完成', validators=[DataRequired()],  choices=[("1", '是'),("0",'否')])
     submit = SubmitField('提交')
 
 
-class LoginForm(FlaskForm):
+class LoginForm(FlaskForm)saf:
     username = StringField('用户名', validators=[DataRequired(), Length(1, 24)])
-    password = PasswordField('密码', validators=[DataRequired(), Length(1, 24)])
-    submit = SubmitField('登录')
+    password = PasswordFiesafsafsafsafsafsa
